0590bedbbe4f676c35ae47dd626698f48f9c2f27
Dominik Paluch <dominik.paluch1995@gmail.com>
2016-07-12 22:52:58 +0200

    add Action Subscribe and Unsubscribe

---
+++ SerwisSubskrypcja/Models/Subscriber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SerwisSubskrypcja.Models
+{
+    public class Subscriber
+    {
+        [ScaffoldColumn(false)]
+        public long  Id { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Email { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
---
+++ SerwisSubskrypcja/Models/SubscriptionContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SerwisSubskrypcja.Models
+{
+    public class SubscriptionContext : DbContext
+    {
+        public SubscriptionContext() : base("SerwisSubskrypcja")
+        {
+
+        }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+    }
+}
